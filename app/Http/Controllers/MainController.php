783776cb0dfcfff36c0a93ee54248c134829331f
StHucher <stephane.hucher40@gmail.com>
2022-08-14 14:16:01 +0200

    Namespace and .env completed

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Routing\Controller;
+
+class MainController extends Controller
+{
+    public function home()
+    {
+        echo " I am here";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MainController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/* Route::get('/', function () {
     return view('welcome');
-});
+}); */
+
+$router->get('/',
+    [
+        'uses' => 'MainController@home',
+        'as' =>'main-home'
+    ]
+);
